ef7d9381717e8c337250707456e49b34aa7d2d71
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-06 11:57:31 +0700

    Initial commit

---
+++ lib/Graphics/ColorNamesCMYK/ToutesLesCouleursCom/Red.pm
@@ -0,0 +1,36 @@
+package Graphics::ColorNamesCMYK::ToutesLesCoulersCom::Red;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# BEGIN_CODE
+#use HashData::Color::CMYK::ToutesLesCouleursCom::Red;
+#
+#print 'our $NAMES_CMYK_TABLE = {', "\n";
+#
+#my $hd = HashData::Color::CMYK::ToutesLesCouleursCom::Red->new;
+#
+#while ($hd->has_next_item) {
+#    my $row = $hd->get_next_item;
+#    my ($c, $m, $y, $k) = split /,/, $row->[1];
+#    print sprintf "  '%s' => 0x%08x, # %d,%d,%d,%d\n", $row->[0], ($c * 256**3 + $m * 256**2 + $y * 256 + $k), $c, $m, $y, $k;
+#}
+#
+#print "};\n\n";
+# END_CODE
+
+1;
+# ABSTRACT: CMYK colors from http://toutes-les-couleurs.com/ (red)
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+Other C<Graphics::ColorNamesCMYK::ToutesLesCoulersCom::*> modules.
+
+Other C<Graphics::ColorNamesCMYK::*> modules.
